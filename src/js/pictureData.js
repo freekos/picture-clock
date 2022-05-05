5ffad45ab1eb08fbe92510045361960aaa26ea91
Freekos <maks.mafia00@mail.ru>
2022-05-05 17:57:21 +0300

    other js logic

---
+++ src/js/pictureData.js
@@ -0,0 +1,15 @@
+const pictureData = [
+    "https://t3.ftcdn.net/jpg/03/92/25/22/240_F_392252254_IGdYtYTHToZX7JKZuPm4jFc2ZfV0xUp5.jpg",
+    "https://t4.ftcdn.net/jpg/04/15/79/09/240_F_415790935_7va5lMHOmyhvAcdskXbSx7lDJUp0cfja.jpg",
+    "https://t3.ftcdn.net/jpg/03/16/51/34/240_F_316513431_HBL4Tgb45Ro1PaiL3dtFs2HRqYuR0tt8.jpg",
+    "https://t4.ftcdn.net/jpg/02/84/36/59/240_F_284365935_RQAZNpqkgzIyCRWibM3a3t2tgwWpkHP1.jpg",
+    "https://t3.ftcdn.net/jpg/04/91/88/20/240_F_491882087_MqpXv3wrrTgR8YVFwrUyebNgIKlKHYsn.jpg",
+    "https://t3.ftcdn.net/jpg/04/18/59/42/240_F_418594255_EcP12yqKgkLUVLXV5ZQQUAATfwtcaQlf.jpg",
+    "https://t4.ftcdn.net/jpg/02/82/33/07/240_F_282330794_x4U2LhXomoKGR9kPKpBSN1qm2b6UCrTz.jpg",
+    "https://t3.ftcdn.net/jpg/04/20/83/62/240_F_420836293_SI0qbXduATyRUZLIQBhb4RPffG4VEEkX.jpg",
+    "https://t4.ftcdn.net/jpg/03/08/76/15/240_F_308761518_wWkPzglX93f0UEBmTvg96SS1crbk99Rv.jpg",
+    "https://t4.ftcdn.net/jpg/03/15/10/35/240_F_315103553_PzFXnvYvpBhFEP2HR99Fi7TWDlU2O7Ze.jpg",
+    "https://t3.ftcdn.net/jpg/02/58/18/06/240_F_258180661_y40ddZDL7IxNK5LuPqXZTPOcbSv08t4x.jpg"
+]
+
+export default pictureData;
---
+++ src/js/timeObj.js
@@ -0,0 +1,7 @@
+const timeObj = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+}
+
+export default timeObj;
